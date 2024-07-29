b8c327d365302f19888989bf4358799919223135
Ignacio Quirós <ignacio.quiros.sordo@gmail.com>
2024-07-29 19:01:17 +0200

    Added my Navbar & Footer components

---
+++ src/js/component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-primary bg-gradient border-top border-5 border-dark footer mt-5 py-3 text-center'>
+            <div>Made by <strong>Ignacio Quirós</strong></div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/js/component/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar container-fluid d-flex justify-content-between navbar-expand-lg navbar-light bg-primary bg-gradient border-bottom border-5 border-dark py-3">
+            <div>
+                <h1 className="navbar-brand ms-2 fs-4" >CONTACT LIST</h1>
+            </div>
+            <div className="m-2">
+                <a href="https://github.com/IgnacioQuiros" className="m-2 text-dark">Github!</a>
+                <a href="https://es.linkedin.com/in/ignacio-quir%C3%B3s-sordo-137781184" className="my-2 text-dark">Linkedin!</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
